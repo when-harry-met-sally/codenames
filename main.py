5af63c1bc06b8672f8783192a8032ac0997abb55
Miles <miles-moran@hotmail.com>
2020-04-30 20:16:31 -0500

    test

--- main.py
+++ main.py
@@ -1,61 +1,21 @@
-import requests
-from pprint import pprint
-from dotenv import load_dotenv
-import os
-import sys
-import itertools 
-load_dotenv()
+import itertools
 
-key =  os.getenv('PROJECT_API_KEY')
+words = ['tree', 'garden', 'olive']
+assoc = [['branch', 'leaves', 'squirrel'],['branch', 'gnome', 'vegetation'],['martini', 'branch', 'sodium']]
 
-def getAssociatedWords(word):
-    res = requests.get('https://api.wordassociations.net/associations/v1.0/json/search?apikey=' + key +'&text=' + word + '&lang=en&limit=300')
-    return res.json()
+print('----')
+match = []
+for i in range(0, len(words)-1):
+    for j in range(i+1, len(words)):
+        print(words[i] + ':',assoc[i])
+        print(words[j] + ':', assoc[j])
+        print('----')
+        m = []
+        for k in assoc[i]:
+            for l in assoc[j]:
+                if (k == l):
+                    m.append(k)
+        match.append(m)
 
-def findCommonWord(words):
-    associated = []
-    for word in words:
-        data = getAssociatedWords(word)
-        items = data['response'][0]['items']
-        associated.append(items)
-
-    best = []
-
-    for i in associated:
-        for j in associated:
-            if i != j:
-                for k in i:
-                    for l in j:
-                        if k['item'] == l['item']:
-                            print(k)
-                            print(l)
-                            print(k['weight'] + l['weight'])
-                            print('-')
-                            if k['weight'] > 20 and l['weight'] > 20:
-                                if k['item'].lower() not in words:
-                                    best.append({'word': k['item'], 'w1': k['weight'], 'w2': l['weight'], 'weight': k['weight'] + l['weight']})
-    print('----Best Results----')
-    for b in best:
-        pprint(b)
-
-def handleArgs():
-    words = []
-    for i in range(1, len(sys.argv)):
-        arg = sys.argv[i]
-        if i != 0:
-            words.append(arg)
-    return words
-
-def findGroupings(words):
-    groupings = []
-    for i in range(1, len(words)):
-        groupings.extend(list(itertools.combinations(words, i+1)))
-    return groupings
-
-def analyzeGroupings(groupings):
-    print(groupings)
-
-if __name__ == "__main__":
-    words = handleArgs()
-    groupings = findGroupings(words)
-    results = analyzeGroupings(groupings)   
+print('----------------------------')
+print(match)
