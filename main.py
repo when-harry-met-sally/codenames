b2b3407afc861945aafd82a0d860ddd3ea59aeca
Miles <miles-moran@hotmail.com>
2020-04-30 23:46:30 -0500

    cleaned p

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ dictionary = {
     "autumn": ['leaves', 'fall', 'season', 'halloween'],
     "pumpkin": ['halloween', 'branch']
 }
-# words = ['tree':{}, 'garden', 'olive']
-# assoc = [['branch', 'leaves', 'squirrel'],['branch', 'gnome', 'vegetation'],['martini', 'branch', 'sodium']]
 
 def findGroupings(words):
     groupings = []
@@ -17,25 +15,6 @@ def findGroupings(words):
         groupings.extend(list(itertools.combinations(words, i+1)))
     return groupings
 
-# print('----')
-# match = []
-# for i in range(0, len(words)-1):
-#     for j in range(i+1, len(words)):
-#         print(words[i] + ':',assoc[i])
-#         print(words[j] + ':', assoc[j])
-#         print('----')
-#         m = []
-#         for k in assoc[i]:
-#             for l in assoc[j]:
-#                 if (k == l):
-#                     m.append(k)
-#         match.append(m)
-
-# print('----------------------------')
-# print(match)
-
-# for grouping in findGroupings(words):
-#     assoc = 
 words = []
 for i in dictionary:
     words.append(i)
