e280f56c1a7d8ac7d67a35edd828eb307c89322c
Miles <miles-moran@hotmail.com>
2020-10-14 07:54:54 -0500

    reworked the JSON structure that is returned through the API in preperation for the react native app

--- solver.py
+++ solver.py
@@ -8,22 +8,22 @@ load_dotenv()
 
 key =  os.getenv('PROJECT_API_KEY') #SORT THAAT WLL FIX SOME BUGS
 def convertToJSON(dictionary):
+    friendly = []
     pprint('--converting to json friendly object--')
-    friendly = {}
-    for i in dictionary:
-        newKey = ''
-        for j in i:
-            newKey += j + ' '
-        friendly[newKey] = list(dictionary[i])
+    for item in list(dictionary.items()):
+        solution = {
+            "words": list(item[0]),
+            "solutions": list(item[1])
+        }
+        friendly.append(solution)
     return friendly
 
 def getAssociatedWords(word):
-    res = requests.get('https://api.wordassociations.net/associations/v1.0/json/search?apikey=' + key +'&text=' + word + '&lang=en&limit=300')
+    # res = requests.get('https://api.wordassociations.net/associations/v1.0/json/search?apikey=' + key +'&text=' + word + '&lang=en&limit=300')
+    res = requests.get('https://api.datamuse.com/words?ml=' + word + '&max=100')
     res = res.json()
-    res = res['response'][0]['items']
+    print(res)
     return res
-
-
 
 def solve(dictionary):
     solution = [i for i in dictionary]
@@ -35,7 +35,7 @@ def solve(dictionary):
             matches = []
             for k in dictionary[a]:
                 for l in dictionary[b]:
-                    if k['item'] == l['item'] and not itemInArray(matches, k['item']):
+                    if k['word'] == l['word'] and not itemInArray(matches, k['word']):
                         matches.append(k)
             solution.append(joined)
             dictionary[joined] = tuple(matches)
@@ -44,6 +44,6 @@ def solve(dictionary):
 
 def itemInArray(array, item):
     for a in array:
-        if a['item'] == item:
+        if a['word'] == item:
             return True
     return False
